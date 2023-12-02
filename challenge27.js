d275e1811f55ad47d47aa3a4cf289c3fb4d9753c
Disha Biswas <112744008+21-0075-disha@users.noreply.github.com>
2023-12-02 13:27:53 +0530

    Create challenge27.js

---
+++ challenge27.js
@@ -0,0 +1,17 @@
+/* Challenge 27:
+Description:
+Write a generator function range that takes two arguments,
+start and end, and yields all the numbers in that range.
+Solution Approach:
+Use the function* syntax for generator functions and the yield keyword.
+*/
+function* range(start, end) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+}
+
+const myRange = range(1, 5);
+for (const num of myRange) {
+  console.log(num);
+}
